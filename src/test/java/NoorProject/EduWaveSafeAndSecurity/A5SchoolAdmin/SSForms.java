3b49a2b8670ad6097460131f6156e60147d872ad
rshawabkeh <alshawabkeh.rami.1987@gmail.com>
2018-04-25 20:12:04 +0300

    ITG

--- src/test/java/NoorProject/EduWaveSafeAndSecurity/A5SchoolAdmin/SSForms.java
+++ src/test/java/NoorProject/EduWaveSafeAndSecurity/A5SchoolAdmin/SSForms.java
@@ -7,11 +7,10 @@ package NoorProject.EduWaveSafeAndSecurity.A5SchoolAdmin;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 import static NoorProject.Other.NoorLogin.browserQA;
 import static NoorProject.Other.NoorLogin.waitQA;
@@ -29,32 +28,53 @@ public class SSForms {
         waitQA.until(ExpectedConditions.visibilityOfElementLocated(SettingMenuLocator)).click();
         waitQA.until(ExpectedConditions.visibilityOfElementLocated(SearchLocator)).click();
         waitQA.until(ExpectedConditions.visibilityOfElementLocated(SSLinkLocator)).click();
-        List TableSize = browserQA.findElements(By.xpath("/html/body/form/div[7]/div[2]/div[2]/div/div/div[1]/div[2]/div/div/table[1]/tbody/tr/td[1]"));
-        if (TableSize.size() <= 1) {
+        WebElement Table = browserQA.findElement(By.id("ctl00_PlaceHolderMain_gvForm"));
+        List Rows = Table.findElements(By.cssSelector("tr[id*='ctl00_PlaceHolderMain_gvForm']"));
+        if (Rows.size() <= 1) {
             System.out.println("يجب نشر نماذج للامن والسلامة");
         } else {
             waitQA.until(ExpectedConditions.visibilityOfElementLocated(ViewLinkLoactor)).click();
         }
         waitQA.until(ExpectedConditions.visibilityOfElementLocated(BTNBackLocator)).click();
-
     }
 
     @Test
     public void editForm() {
-
         waitQA.until(ExpectedConditions.visibilityOfElementLocated(SettingMenuLocator)).click();
         waitQA.until(ExpectedConditions.visibilityOfElementLocated(SearchLocator)).click();
         waitQA.until(ExpectedConditions.visibilityOfElementLocated(SSLinkLocator)).click();
+
         WebElement Table = browserQA.findElement(By.id("ctl00_PlaceHolderMain_gvForm"));
-
         List Rows = Table.findElements(By.cssSelector("tr[id*='ctl00_PlaceHolderMain_gvForm']"));
-       By gg = By.id("ctl00_PlaceHolderMain_gvForm_ctl19_lbtnEditForm");
-        browserQA.findElement(gg).getAttribute("disabled"); //"true , false , null"
-        System.out.println(browserQA.findElement(gg).getAttribute("disabled"));
+        if (Rows.size() <= 1) {
+            System.out.println("يجب نشر نماذج للامن والسلامة");
+        } else {
+            for (int i = 2; i < Rows.size() + 1; i++) {
+                String IdFormat = String.format("%02d" , i);
 
+                By EditLinkLocator = By.id("ctl00_PlaceHolderMain_gvForm_ctl" + IdFormat + "_lbtnEditForm");
+                boolean A1 = Boolean.parseBoolean(browserQA.findElement(EditLinkLocator).getAttribute("disabled"));
+
+                String B1 = String.valueOf(A1);
+
+                if (B1.equals("false")) {
+                    waitQA.until(ExpectedConditions.visibilityOfElementLocated(EditLinkLocator)).click();
+                    browserQA.findElement(By.id("ctl00_PlaceHolderMain_ibtnNext")).click();
+                    browserQA.findElement(By.id("ctl00_ibtnYes")).click();
+
+                    waitQA.until(ExpectedConditions.visibilityOfElementLocated(By.id("ctl00_PlaceHolderMain_lblOpertioanlResult")));
+                    String ActualResult = browserQA.findElement(By.id("ctl00_PlaceHolderMain_lblOpertioanlResult")).getText();
+                    String ExpecteResult = "تمت عملية حفظ البيانات بنجاح.";
+                    Assert.assertEquals(ActualResult , ExpecteResult , "لم تتم عملية الحفظ بنجاح");
+
+                    browserQA.findElement(By.id("ctl00_PlaceHolderMain_ibtnBack")).click();
+                    break;
+                } else {
+                    System.out.println("رابط التعديل غير فعال الرجاء اعتماد النموذج من المؤول المدرسي");
+                }
+
+
+            }
+        }
     }
-
 }
-
-
-
